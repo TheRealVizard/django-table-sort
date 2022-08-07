7428794d4ccde5589c7965f9e838199db7da8e83
TheRealVizard <vizard@divineslns.com>
2022-08-07 02:27:53 -0400

    HotFix: Fixing version

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ project = "django-table-sort"
 author = "Eduardo Leyva"
 copyright = f"{datetime.now().year}, {author}"
 
-version = "0.2.7"
+version = "0.3.1"
 
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx"]
