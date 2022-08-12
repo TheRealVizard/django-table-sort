8056dae69a3a3b76abc8807770fe29d68f610b2c
TheRealVizard <vizard@divineslns.com>
2022-08-12 02:52:47 -0400

    Added fields and exclude test

--- tests/table_test.py
+++ tests/table_test.py
@@ -1,8 +1,9 @@
-from django.test import TestCase
-from django_table_sort.table import TableSort
-
-from tests.models import Person
 from django.test import RequestFactory
+from django.test import TestCase
+
+from django_table_sort.table import TableSort
+from tests.models import Person
+
 
 class Test(TestCase):
     @classmethod
@@ -44,4 +45,32 @@ class Test(TestCase):
         self.assertIn(("age", "Age In Years"), table_columns)
         result = table.render()
         self.assertIn(self.person.name, result)
+        self.assertIn(str(self.person.age), result)
+
+    def test_table_fields(self):
+        table = TableSort(
+            request=self.request, object_list=Person.objects.all(), fields=["name"]
+        )
+        table_columns = [
+            (column.column_field, column.column_header) for column in table.column_names
+        ]
+        self.assertEqual(len(table_columns), 1)
+        self.assertIn(("name", "Full Name"), table_columns)
+        self.assertNotIn(("age", "Age In Years"), table_columns)
+        result = table.render()
+        self.assertIn(self.person.name, result)
+        self.assertNotIn(str(self.person.age), result)
+
+    def test_table_exclude(self):
+        table = TableSort(
+            request=self.request, object_list=Person.objects.all(), exclude=["name"]
+        )
+        table_columns = [
+            (column.column_field, column.column_header) for column in table.column_names
+        ]
+        self.assertEqual(len(table_columns), 1)
+        self.assertNotIn(("name", "Full Name"), table_columns)
+        self.assertIn(("age", "Age In Years"), table_columns)
+        result = table.render()
+        self.assertNotIn(self.person.name, result)
         self.assertIn(str(self.person.age), result)
