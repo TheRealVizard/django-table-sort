06fc54c8e5b55c79da99e1eb5695e206cca05056
TheRealVizard <vizard@divineslns.com>
2022-08-12 02:50:54 -0400

    Added basic table test case

--- tests/settings.py
+++ tests/settings.py
@@ -8,8 +8,39 @@ INSTALLED_APPS = (
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.sites",
+    "django.contrib.messages",
     "tests",
 )
 
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
 USE_TZ = True
+
 SECRET_KEY = "foobar"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    }
+]
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
+    },
+}
--- tests/table_test.py
+++ tests/table_test.py
@@ -1,16 +1,16 @@
-from unittest import TestCase
-
-from django.db import models
-
+from django.test import TestCase
 from django_table_sort.table import TableSort
 
-
-class Person(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Full Name")
-    age = models.IntegerField(verbose_name="Age in years")
-
+from tests.models import Person
+from django.test import RequestFactory
 
 class Test(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.person = Person.objects.create(name="John Doe", age=23)
+        cls.request_factory = RequestFactory()
+        cls.request = cls.request_factory.get("")
+
     def test_table_class(self):
         table = TableSort(
             request=None,
@@ -30,3 +30,18 @@ class Test(TestCase):
         self.assertEqual(len(table_columns), 2)
         self.assertIn(("name", "Full Name"), table_columns)
         self.assertIn(("age", "Age In Years"), table_columns)
+
+    def test_table_basic(self):
+        table = TableSort(
+            request=self.request,
+            object_list=Person.objects.all(),
+        )
+        table_columns = [
+            (column.column_field, column.column_header) for column in table.column_names
+        ]
+        self.assertEqual(len(table_columns), 2)
+        self.assertIn(("name", "Full Name"), table_columns)
+        self.assertIn(("age", "Age In Years"), table_columns)
+        result = table.render()
+        self.assertIn(self.person.name, result)
+        self.assertIn(str(self.person.age), result)
